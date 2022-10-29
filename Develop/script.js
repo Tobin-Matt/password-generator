d29b6169f855a5715f047a8bf3c3b6f5660e09b4
mbtobin <tobin.matthew94@gmail.com>
2022-10-28 18:04:04 -0700

    set criteria variables and condition for password length

--- Develop/script.js
+++ Develop/script.js
@@ -4,17 +4,23 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   //prompt user for password characteristics
-  var numCharacters      = prompt("How many characters would you like your password to be?")
-  var doesUseLowerCase   = confirm("Would you like lower case letters in your password?")
-  var doesUseUpperCase   = confirm("Would you like upper case letters in your password?")
-  var doesUseNumbers     = confirm("Would you like to use numbers in your password?")
-  var doesUseSpecialChar = confirm("Would you like to use special characters in your password?")
+  var numCharacters = prompt("How many number of characters would you like your password to be?")
+  
+  if (numCharacters <= 8 || numCharacters >= 128) {
+    alert("Please input a number between 8 and 128!")
+    return;
+  }
+
+  var useLowerCase   = confirm("Would you like lower case letters in your password?")
+  var useUpperCase   = confirm("Would you like upper case letters in your password?")
+  var useNumbers     = confirm("Would you like to use numbers in your password?")
+  var useSpecialChar = confirm("Would you like to use special characters in your password?")
 
   //assign arrays to variables
-  var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-  var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers          = "0123456789";
-  var specCharacters   = "!@#$%&";
+  var lowerCase   = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers     = "0123456789";
+  var specialChar = "!@#$%&";
 
   // add users selction together to fit the users choice of characters
   
@@ -23,8 +29,7 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
-  generatePassword();
-  var password = generatePassword(); //the main part of the code will live in this generatePassword() function
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
